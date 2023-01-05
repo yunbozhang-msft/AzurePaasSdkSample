898e42d5e8bc6f2adcf320f6b1a0c3fbab0c81f3
yunbozhang <zhang_ybo@outlook.com>
2023-01-05 10:35:55 +0800

    refactor

--- Base-Module/src/main/java/org/common/base/SystemParams.java
+++ Base-Module/src/main/java/org/common/base/SystemParams.java
@@ -1,2 +1,23 @@
-package org.common.base;public class SystemParams {
+package org.common.base;
+
+public class SystemParams {
+    // Eventhub-demo
+    public static String EventhubDemoStr1 = "";
+    public static String EventhubDemoStr2 = "";
+
+    public static String EventhubDemoStr3 = "";
+
+    // Key-Vault-demo
+    public static String KeyVaultDemoStr1 = "";
+    public static String KeyVaultDemoStr2 = "";
+    public static String KeyVaultDemoStr3 = "";
+
+
+
+    // ServiceBusAADTest
+    public static String ServiceBusAADTestStr1 = "";
+    public static String ServiceBusAADTestStr2 = "";
+    public static String ServiceBusAADTestStr3 = "";
+
+
 }
